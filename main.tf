5402c1339a0a590c4caf05bc090967c38b54135d
Rohit Dixit <dixitrohit17@hotmail.com>
2025-07-01 23:59:38 +0530

    .

--- main.tf
+++ main.tf
@@ -15,17 +15,17 @@ resource "aws_s3_bucket" "example" {
     Environment = "dev"
   }
 }
-resource "dynamodb_table" "terraform_lock_table" {
-  name         = "terraform-lock-table"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+# resource "dynamodb_table" "terraform_lock_table" {
+#   name         = "terraform-lock-table"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  tags = {
-    Name        = "Terraform Lock Table"
-    Environment = "dev"
-  }
-}
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+#   tags = {
+#     Name        = "Terraform Lock Table"
+#     Environment = "dev"
+#   }
+# }
