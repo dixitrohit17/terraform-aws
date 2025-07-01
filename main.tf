6d26b314169c30bd5d256eba37a5fce0c909c4be
Rohit Dixit <dixitrohit17@hotmail.com>
2025-07-01 21:29:53 +0530

    s3 Bucket Code Added.

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
-module "aws_vm" {
-    source = "./modules/vm"
-    aws_key_pair_name = var.aws_key_pair_name
-    ami_id = var.ami_id
-    instance_type = var.instance_type
-    vm_name = local.vm_name
-    instance_count = var.instance_count
-}  
+# module "aws_vm" {
+#     source = "./modules/vm"
+#     aws_key_pair_name = var.aws_key_pair_name
+#     ami_id = var.ami_id
+#     instance_type = var.instance_type
+#     vm_name = local.vm_name
+#     instance_count = var.instance_count
+# }  
+
+resource "aws_s3_bucket" "example" {
+  bucket = local.storage_name
+  #region = "ap-south-1"
+
+  tags = {
+    Name        = "POC S3 Bucket"
+    Environment = "dev"
+  }
+}
