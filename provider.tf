5411fc3f7c47bd67bb324774387eac38ad56d5f8
Rohit Dixit <41516577+dixitrohit17@users.noreply.github.com>
2025-07-01 19:54:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "dev-s3-ap-south-1"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+  }
 }
 # Configure the AWS Provider
 provider "aws" {
